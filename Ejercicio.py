b80696901016f6e64aaa5aaf882317eba811fab9
MiloMcNish <100816914+MiloMcNish@users.noreply.github.com>
2022-07-19 19:30:21 -0500

    Update Ejercicio.py

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,4 +1,6 @@
 
+#Parte A 1.Crear una función con tres parámetros que sean números que se suman entre sí. 2. Llamar a la función en el main y darle valores.
+
 def sumar (a,b,c):
     sumar = a+b+c
     print(f"Hola, la suma de {a} + {b} + {c} es: " +  str(sumar))
@@ -10,7 +12,9 @@ print("===== Bienvenido a ======")
 print("=====  tu garage   ======")
 print("=========================")
 
-
+#Parte B 1. Crear una clase coche 2. Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+#3. Una función que incremente el número de puertas que tiene el coche. 4. Crear un objeto miCoche en el main y añadirle una puerta.
+#5. Mostrar el número de puertas que tiene el objeto.
 
 
 class coche:
@@ -26,6 +30,9 @@ class coche:
 miCoche = coche(4)
 
 print("Tu coche tiene: " + str(miCoche.NumPuertas) + " puertas \n")
+
 puertasAumentadas = int(input("Cuantas Puertas va a aumentar?: " +"\n"))
+
 miCoche.aumentarPuertas(puertasAumentadas)
+
 print("\nAhora este es el número de puertas:" + str(miCoche.NumPuertas) )
