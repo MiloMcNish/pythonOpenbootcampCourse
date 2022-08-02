b992c0919fcc43553374cb272005dd6799fde52f
MiloMcNish <100816914+MiloMcNish@users.noreply.github.com>
2022-08-02 12:07:12 -0500

    EjercicioGUI

--- ejercicioGUI4.py
+++ ejercicioGUI4.py
@@ -10,8 +10,10 @@ window.columnconfigure(1,weight=4)
 lista = ['windows','mac','linux','ms dos']
 lista_items= tkinter.StringVar(value=lista)
 
-listbox=tkinter.Listbox(window,height=50,width=50, listvariable=lista_items)
-listbox.grid(column=0,row=0,sticky=tkinter.W)
+label = tkinter.Label(window,text='Lista de opciones')
+listbox=tkinter.Listbox(window,height=10,width=50, listvariable=lista_items)
+listbox.grid(column=0,row=1,sticky=tkinter.W)
+label.grid(column=0,row=0, sticky=tkinter.W)
 
 
 window.mainloop()
