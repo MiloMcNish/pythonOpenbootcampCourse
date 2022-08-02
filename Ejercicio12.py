e6da32c33fc4b4e43d7074fa81e51bb168049dae
MiloMcNish <100816914+MiloMcNish@users.noreply.github.com>
2022-08-01 21:24:50 -0500

    Ejercicio12

--- Ejercicio12.py
+++ Ejercicio12.py
@@ -1,4 +1,5 @@
 from operator import truediv
+from functools import reduce
 
 
 lista =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
@@ -7,9 +8,16 @@ def impar(x):
         return False
     else:
         return True
-
+def suma(a,b):
+    return a+b
 def impares(x):
     imparL =filter(impar,lista)
+    print(list(imparL))
+    imparL =filter(impar,lista)
+    imparL = reduce(suma,imparL)
+    
+    print(imparL)
     return imparL
+    
 
-print(list(impares(lista)))
+impares(lista)
